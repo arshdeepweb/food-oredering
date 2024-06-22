c575ccd9b153a5dc6da88f962e570aac81b38be0
Arshdeep Singh <imarshdeep99@gmail.com>
2024-06-22 22:52:41 +0530

    Fix Header Image

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import '../../App.css'
+
 function Header() {
 
 
 
   return (
     <>
-      <div className='bg-[url(../.././public/header_img.png)] h-[60vh] relative bg-cover flex items-center bg-center rounded-xl bg-no-repeat'>
+      <div className='bg-[url(/header_img.png)] h-[60vh] relative bg-cover flex items-center bg-center rounded-xl bg-no-repeat'>
         <div className='flex flex-col px-8 text-center sm:text-left w-[100%] gap-2 z-10 relative sm:w-[60%]'>
+          <img src="./" alt="" />
           <h2 className='text-5xl text-white font-bold font-sans'>The Best Food Place is Here</h2>
           <p className='text-xl text-white font-bold font-sans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur, voluptatem ullam error architecto dicta!</p>
           <button className='text-white w-[8rem] border-2 border-[#ec3131] hover:bg-[#ec3131] hover:text-white border-solid bg-transparent rounded-2xl py-2 px-5'>View More</button>
